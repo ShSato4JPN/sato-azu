94651562e0195aede4fb5c4464ddb79a53a1bcd2
ShSato4JPN <shsato4.private.1221@gmail.com>
2023-03-28 22:54:30 +0900

    feat: add endpoints

---
+++ src/libs/endpoinsts/index.ts
@@ -0,0 +1,5 @@
+export const endpointProfile = `${process.env.NEXT_PUBLIC_URL}/api/contents/about`;
+export const endpointLifeStyle = `${process.env.NEXT_PUBLIC_URL}/api/contents/lefestyle`;
+export const endpointMessage = `${process.env.NEXT_PUBLIC_URL}/api/contents/message`;
+export const endpointStory = `${process.env.NEXT_PUBLIC_URL}/api/contents/story`;
+export const endpointTimeLine = `${process.env.NEXT_PUBLIC_URL}/api/contents/timeline`;
